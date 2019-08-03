63f9c042478a96630fe452b04e9504bf523c52be
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-03 12:30:27 -0500

    userdata.sh added

--- isntance.tf
+++ isntance.tf
@@ -1,18 +1,18 @@
 resource "aws_instance" "web1" {
 
-  ami                         = ""
-  instance_type               = ""
-  key_name                    = ""
-  subnet_id                   = ""
-  vpc_security_group_ids      = ""
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  subnet_id                   = "${aws_subnet.public.id}"
+  vpc_security_group_ids      = "[${aws_security_group.public.id}]"
   associate_public_ip_address = "true"
-  user_data                   = ""
-  count                       = ""
+  user_data                   = "${var.userdata}"
+  count                       = "${var.count}"
 
 
 
   tags = {
-    Name      = "${var.Name}.public"
+    Name      = "${var.Name}${count.index + 1}"
     Env       = "${var.Env}"
     Create_by = "${var.Created_by}"
     Dept      = "${var.Dept}"
