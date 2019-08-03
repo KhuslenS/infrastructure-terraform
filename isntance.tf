ea6c018be97859cbfe9f7e221dab0e1625f4f087
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-03 12:38:03 -0500

    updated

--- isntance.tf
+++ isntance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web1" {
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.key_name}"
   subnet_id                   = "${aws_subnet.public.id}"
-  vpc_security_group_ids      = "[${aws_security_group.public.id}]"
+  vpc_security_group_ids      = ["${aws_security_group.public.id}"]
   associate_public_ip_address = "true"
   user_data                   = "${file("userdata.sh")}"
 
